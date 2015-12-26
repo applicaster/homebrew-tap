74c0186d73cb0bc0c7dee252cd793210098e90c1
Philip Kramarov <p.kramarov@applicaster.com>
2015-12-26 21:32:07 +0200

    Test

--- zapptool.rb
+++ zapptool.rb
@@ -1,21 +1,25 @@
-class Rar < Formula
-  desc "archive manager for RAR/ZIP file formats"
-  homepage "http://www.rarlab.com"
-  url "http://www.rarlab.com/rar/rarosx-5.2.1.tar.gz"
-  sha256 "78f023dc1ba1d95f5c2fb5b90c16f214c8bfd2973bf79d92b8a523d711a57065"
+class ZappTool < Formula
+  desc "Drop-in replacement for xcodebuild with a few extra features"
+  homepage "https://github.com/facebook/xctool"
+  url "https://github.com/facebook/xctool/archive/0.2.7.tar.gz"
+  sha256 "afbc829f6b9b3f9d45755cd33a8b171def67dd9733977f70a0638d69f4a08730"
+  head "https://github.com/facebook/xctool.git"
 
-  bottle :unneeded
-
-  resource "man" do
-    url "http://manpages.ubuntu.com/manpages.gz/trusty/man1/rar.1.gz"
-    sha256 "9a2dd38c3ec1e098f29b720116ad77a71e700dccebaa936cb7ace397702b287a"
+  bottle do
+    cellar :any
+    sha256 "4096a879ff3553b5c07077c3d3797aae526ced093f8235377a8f0e3cd2645f8a" => :el_capitan
+    sha256 "5c2033c60a8a1c7fa364ff46e5a6e432207ee0323793ad851cef67b9379aa9c6" => :yosemite
+    sha256 "22af6fd008025eb106f775fe44d3ed3afa3e961ee3872c54f26849c2866bcf3b" => :mavericks
   end
+
+  depends_on :xcode => "6.0"
 
   def install
-    bin.install "rar", "unrar"
-    lib.install "default.sfx"
-    etc.install "rarfiles.lst"
-    doc.install "acknow.txt", "order.htm", "rar.txt", "whatsnew.txt"
-    man1.install resource("man")
+    system "./scripts/build.sh", "XT_INSTALL_ROOT=#{libexec}", "-IDECustomDerivedDataLocation=#{buildpath}"
+    bin.install_symlink "#{libexec}/bin/xctool"
   end
-end 
+
+  test do
+    system "(#{bin}/xctool -help; true)"
+  end
+end
