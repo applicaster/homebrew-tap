593f9d0a64900749554cec41f3bc4aaad71d33ca
Philip Kramarov <p.kramarov@applicaster.com>
2015-12-26 20:31:03 +0200

    Changed zip file to tar.gz

--- zapptool.rb
+++ zapptool.rb
@@ -1,6 +1,6 @@
 class ZappTool < Formula
   homepage "https://github.com/applicaster/Zapp-iOS-App-Configuration-Tool"
-  url "https://github.com/applicaster/Zapp-iOS-App-Configuration-Tool/releases/download/0.1.1/ZappTool-0.1.1.zip"
+  url "https://github.com/applicaster/Zapp-iOS-App-Configuration-Tool/releases/download/0.1.1/ZappTool-0.1.1.tar.gz"
   sha1 ""
 
   bottle :unneeded
