97f95971447a75c284157d085d817bf63df33050
Philip Kramarov <p.kramarov@applicaster.com>
2015-12-26 19:42:09 +0200

    Created ZappTool Formula

---
+++ ZappTool.rb
@@ -0,0 +1,8 @@
+class ZappTool < Formula
+  homepage ""
+  url "https://github.com/applicaster/Zapp-iOS-App-Configuration-Tool/releases/download/0.1.1/ZappTool-0.1.1.zip"
+  sha1 ""
+
+  def install
+  end
+end
