887eccadbfc77a14737bb2ed4f1db4e7e03df984
Philip Kramarov <p.kramarov@applicaster.com>
2015-12-28 11:27:55 +0200

    Updated Formula

--- zapptool.rb
+++ zapptool.rb
@@ -1,9 +1,19 @@
 class Zapptool < Formula
+  desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/Zapp-iOS-App-Configuration-Tool"
-  url "https://github.com/applicaster/Zapp-iOS-App-Configuration-Tool/releases/download/0.1.1/ZappTool-0.1.1.tar.gz"
-  sha1 ""
+  url "https://github.com/applicaster/Zapp-iOS-App-Configuration-Tool.git"
+
+  depends_on :xcode => "7.1"
 
   def install
-    bin.install "zapptool"
+    system "./scripts/build.sh", "SYMROOT=build"
+    prefix.install Dir["build/Release/*"]
+    inner_binary = "#{prefix}/ZappConfiguration"
+    bin.write_exec_script inner_binary
+    chmod 0755, bin/"ZappConfiguration"
+  end
+
+  test do
+    system "(#{bin}/ZappConfiguration -version; true)"
   end
 end
