b51d12e9d4d5dbbc42f222f9151556812fe92dc7
Philip Kramarov <p.kramarov@applicaster.com>
2016-01-08 17:18:39 +0200

    Update zapptool.rb

--- zapptool.rb
+++ zapptool.rb
@@ -2,9 +2,10 @@ class Zapptool < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git
+  head "353c755e2ea81bd8e1fd4ff074672f0db0c33327"
   version "0.1.4"
-
-  depends_on :xcode => "7.1"
+  
+  depends_on :xcode => "7.2"
 
   def install
     system "./scripts/build.sh", "SYMROOT=build"
