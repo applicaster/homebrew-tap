fa1c5799085c9f55e8a7af11b92c0bf711c72f1d
Philip Kramarov <p.kramarov@applicaster.com>
2016-02-16 17:48:20 +0200

    Update zapptool.rb

--- zapptool.rb
+++ zapptool.rb
@@ -2,16 +2,16 @@ class Zapptool < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git,
-                                                 :tag => "v0.1.4"
+                                                 :tag => "v0.1.6"
   head "https://github.com/applicaster/ZappTool.git"
 
-  version "0.1.4"
+  version "0.1.6"
 
   depends_on :xcode => "7.1"
 
   def install
-    system "./scripts/build.sh", "SYMROOT=build"
-    prefix.install Dir["build/Release/*"]
+    system "OUTPUT_DIR=#{buildpath} ./scripts/build.sh"
+    prefix.install Dir["#{buildpath}/build/zapptool"]
     inner_binary = "#{prefix}/zapptool"
     bin.write_exec_script inner_binary
     chmod 0755, bin/"zapptool"
