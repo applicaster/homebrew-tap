b390560ff87a2f0e815086df8a5171babb1a3a32
ranmeirman <r.meirman@applicaster.com>
2016-05-09 23:39:05 -0400

    update zappifest sha (#4)

--- zappifest.rb
+++ zappifest.rb
@@ -3,7 +3,7 @@ class Zappifest < Formula
   homepage "https://github.com/applicaster/zappifest"
   url "https://github.com/applicaster/zappifest/archive/0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "37d2ff88f9ae7cd7ddebbf117f2a88ebb9052ce72df8413b230f713e1c6518ea"
+  sha256 "adbdb7f77ba5fc5dc4b976d1846b2cda7e6d24a2041e3d81e9bc1b7776ec3a7f"
 
   def install
     bin.install "zappifest"
