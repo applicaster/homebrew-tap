2a69794eb539098494307d59e526fab898920fda
ranmeirman <r.meirman@applicaster.com>
2016-05-10 00:10:29 -0400

    update zappifest to 0.1.1 (#5)

--- zappifest.rb
+++ zappifest.rb
@@ -1,9 +1,9 @@
 class Zappifest < Formula
   desc "Tool to generate Zapp plugin manifest"
   homepage "https://github.com/applicaster/zappifest"
-  url "https://github.com/applicaster/zappifest/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "adbdb7f77ba5fc5dc4b976d1846b2cda7e6d24a2041e3d81e9bc1b7776ec3a7f"
+  url "https://github.com/applicaster/zappifest/archive/0.1.1.tar.gz"
+  version "0.1.1"
+  sha256 "f15d7f9f3b97fb53bf38bee2297d11858d4443d854f015789e8eb7d4d9429926"
 
   def install
     bin.install "zappifest"
