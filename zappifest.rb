40382c579a09fdb43c5098b37f160e13d02b1d6b
Ran Meirman <r.meirman@applicaster.com>
2016-05-13 16:11:24 -0400

    update zappifest to use 0.2.2

--- zappifest.rb
+++ zappifest.rb
@@ -1,12 +1,12 @@
 class Zappifest < Formula
   desc "Tool to generate Zapp plugin manifest"
   homepage "https://github.com/applicaster/zappifest"
-  url "https://github.com/applicaster/zappifest/archive/0.2.1.tar.gz"
-  version "0.2.1"
-  sha256 "fb787512c8c4443229c1635d84cab7cce49ea4c17e52470dc7fc5a20ca140c48"
+  url "https://github.com/applicaster/zappifest/archive/0.2.2.tar.gz"
+  version "0.2.2"
+  sha256 "e3ef8466b204d9a849fd72d61ff868122ddfc4926542202c361fc516572cda5c"
 
   def install
-    prefix.install Dir["output/*"]
-    bin.install "zappifest"
+    lib.install "lib/multipart.rb", "lib/network_helpers.rb"
+    bin.install "bin/zappifest"
   end
 end
