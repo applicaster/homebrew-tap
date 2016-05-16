08870920e8e9d6e1657ff669ef8236fe96c8cf7b
ranmeirman <r.meirman@applicaster.com>
2016-05-16 17:03:51 -0400

    update zappifest formula (#10)

--- zappifest.rb
+++ zappifest.rb
@@ -1,9 +1,9 @@
 class Zappifest < Formula
   desc "Tool to generate Zapp plugin manifest"
   homepage "https://github.com/applicaster/zappifest"
-  url "https://github.com/applicaster/zappifest/archive/0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "8a6410c57219fc1753913a7f00ee9f41d06172f26e268e89989de1581fbefd29"
+  url "https://github.com/applicaster/zappifest/archive/0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "397f69d6fe903d7f6e92fab0c076e476e70006b2ed71619cb77ecf783f2f7789"
 
   def install
     lib.install "lib/multipart.rb", "lib/network_helpers.rb"
