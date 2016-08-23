4a093a6281adfa29def1d86b7685d212934e68c6
Liviu R <livius16@gmail.com>
2016-08-23 12:16:54 -0400

    Update Zapptool to 0.2.7 - Fix WhatsApp sharing

--- zapptool.rb
+++ zapptool.rb
@@ -2,10 +2,10 @@ class Zapptool < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git,
-                                                 :tag => "v0.2.6"
+                                                 :tag => "v0.2.7"
   head "https://github.com/applicaster/ZappTool.git"
 
-  version "0.2.6"
+  version "0.2.7"
 
   depends_on :xcode => "7.3"
 
