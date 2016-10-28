2cd5692ee84d9105d77df066f1536750886e5ca1
Ran Meirman <r.meirman@applicaster.com>
2016-10-28 16:32:01 -0400

    update zappifest

--- zappifest.rb
+++ zappifest.rb
@@ -1,12 +1,39 @@
 class Zappifest < Formula
   desc "Tool to generate Zapp plugin manifest"
   homepage "https://github.com/applicaster/zappifest"
-  url "https://github.com/applicaster/zappifest/archive/0.11.0.tar.gz"
-  version "0.11.0"
-  sha256 "b8b90011b2ff5c4d7a1fd0a385dd072e2dcfe76427b42f48301eebc9076bb51e"
+  url "https://github.com/applicaster/zappifest/archive/0.13.2.tar.gz"
+  version "0.13.2"
+  sha256 "d4031973c80efe5a89536b7b69e94ce55508d5120d05627662c8566d920bbb1d"
+
+  resource "commander" do
+    url "https://rubygems.org/gems/commander-4.4.0.gem"
+    sha256 "358ae12110faed47683633bdf0e71d6782a0f3eccdbaea4e76b375d260490a56"
+  end
+
+  resource "diffy" do
+    url "https://rubygems.org/gems/diffy-3.1.0.gem"
+    sha256 "72161a619117127af958d3fca3d04a1f6dd6c5bb416209ff923aa4ed7e5cf1ab"
+  end
+
+  resource "terminal-table" do
+    url "https://rubygems.org/gems/terminal-table-1.7.3.gem"
+    sha256 "f10d5ff88f5596dfeb45d2cdf1ad35b8b8e347f58bcc7307a270af44176885a5"
+  end
 
   def install
-    lib.install "lib/multipart.rb", "lib/network_helpers.rb"
-    bin.install "bin/zappifest"
+    resources.each do |r|
+      r.verify_download_integrity(r.fetch)
+      system("gem", "install", r.cached_download, "--no-document",
+        "--install-dir", "#{libexec}/vendor")
+    end
+
+    mkpath bin
+    (bin/"zappifest").write <<-EOS.undent
+    #!/bin/bash
+    export GEM_HOME="#{libexec}/vendor"
+    ruby #{lib}/zappifest.rb "$@"
+    EOS
+
+    lib.install "lib/multipart.rb", "lib/network_helpers.rb", "lib/zappifest.rb"
   end
 end
