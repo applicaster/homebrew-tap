8b2b6fdad3a5f4fd9725f2736023df7c74a300ba
ranmeirman <r.meirman@applicaster.com>
2016-10-27 22:04:29 -0400

    update zappifest (#28)

--- zappifest.rb
+++ zappifest.rb
@@ -5,6 +5,16 @@ class Zappifest < Formula
   version "0.13.0"
   sha256 "df5b8ec3259ad8b9345c21bfd5d3fc35aed992e6ea823705e549c0b4451c60df"
 
+  resource "commander" do
+    url "https://rubygems.org/gems/commander-4.4.0.gem"
+    sha256 "358ae12110faed47683633bdf0e71d6782a0f3eccdbaea4e76b375d260490a56"
+  end
+
+  resource "diffy" do
+    url "https://rubygems.org/gems/diffy-3.1.0.gem"
+    sha256 "72161a619117127af958d3fca3d04a1f6dd6c5bb416209ff923aa4ed7e5cf1ab"
+  end
+
   resource "terminal-table" do
     url "https://rubygems.org/gems/terminal-table-1.7.3.gem"
     sha256 "f10d5ff88f5596dfeb45d2cdf1ad35b8b8e347f58bcc7307a270af44176885a5"
