8382e264bbd8d8700cbc8178cccc5988a68c9ba8
Alex Zchut <alexzchut@users.noreply.github.com>
2017-01-18 11:02:05 +0200

    Update zapptool.rb
    update zapptool to 2.14

--- zapptool.rb
+++ zapptool.rb
@@ -2,10 +2,10 @@ class Zapptool < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git,
-                                                 :tag => "v0.2.13"
+                                                 :tag => "v0.2.14"
   head "https://github.com/applicaster/ZappTool.git"
 
-  version "0.2.13"
+  version "0.2.14"
 
   depends_on :xcode => "8.1"
 
