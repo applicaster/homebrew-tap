6f54f051bbaab3f064a97c96392656423947bc8d
ranmeirman <r.meirman@applicaster.com>
2017-02-09 16:44:46 -0500

    update zappifest (#52)

--- zappifest.rb
+++ zappifest.rb
@@ -1,9 +1,9 @@
 class Zappifest < Formula
   desc "Tool to generate Zapp plugin manifest"
   homepage "https://github.com/applicaster/zappifest"
-  url "https://github.com/applicaster/zappifest/archive/0.18.0.tar.gz"
-  version "0.18.0"
-  sha256 "cfefa3dac64f1fc1b8db6158593dc27496fd7517fe7272d8d06bf12be006b7db"
+  url "https://github.com/applicaster/zappifest/archive/0.19.0.tar.gz"
+  version "0.19.0"
+  sha256 "2f4981320031a7058d1c7c4df042935cb2077af6f24ad2a0648a59ce7bee2833"
 
   resource "commander" do
     url "https://rubygems.org/gems/commander-4.4.0.gem"
@@ -18,6 +18,11 @@ class Zappifest < Formula
   resource "terminal-table" do
     url "https://rubygems.org/gems/terminal-table-1.7.3.gem"
     sha256 "f10d5ff88f5596dfeb45d2cdf1ad35b8b8e347f58bcc7307a270af44176885a5"
+  end
+
+  resource "inquirer" do
+    url "https://rubygems.org/gems/inquirer-0.2.1.gem"
+    sha256 "1cb639228bf9f2e98543f866e94e64872631d7b6c8b2a5565289175405e1e0af"
   end
 
   def install
