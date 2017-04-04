d047e4b8d024e5587c3ed339ff40fcfb384b2210
kononenkoAnton <a.kononenko@applicaster.com>
2017-04-04 22:04:42 +0300

    Update zapptool.rb

--- zapptool.rb
+++ zapptool.rb
@@ -2,10 +2,10 @@ class Zapptool < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git,
-                                                 :tag => "v0.3.2"
+                                                 :tag => "v0.3.3"
   head "https://github.com/applicaster/ZappTool.git"
 
-  version "0.3.2"
+  version "0.3.3"
 
   depends_on :xcode => "8.1"
 
