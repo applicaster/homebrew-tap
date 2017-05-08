f0e309876fe8375a8bef17f126b45847af7eae02
Alex Zchut <alexzchut@users.noreply.github.com>
2017-05-08 21:15:06 +0300

    Update zapptool.rb

--- zapptool.rb
+++ zapptool.rb
@@ -2,10 +2,10 @@ class Zapptool < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git,
-                                                 :tag => "v0.3.6"
+                                                 :tag => "v0.3.7"
   head "https://github.com/applicaster/ZappTool.git"
 
-  version "0.3.6"
+  version "0.3.7"
 
   depends_on :xcode => "8.1"
 
