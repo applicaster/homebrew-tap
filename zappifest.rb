1d734c0524215e63c4f98dc30cd0a6bd7bd54f7c
ranmeirman <r.meirman@applicaster.com>
2017-06-12 08:33:25 -0400

    update zappifest (#79)

--- zappifest.rb
+++ zappifest.rb
@@ -1,9 +1,9 @@
 class Zappifest < Formula
   desc "Tool to generate Zapp plugin manifest"
   homepage "https://github.com/applicaster/zappifest"
-  url "https://github.com/applicaster/zappifest/archive/0.27.1.tar.gz"
-  version "0.27.1"
-  sha256 "283a1ef4d62b1727e99e0f357a48a729844c3f087ba27da18fe9cd95d9a17820"
+  url "https://github.com/applicaster/zappifest/archive/0.27.2.tar.gz"
+  version "0.27.2"
+  sha256 "884ab4d3b283002b3f9993bf18bed2ddc9299661ecba20d8104d86ba6c4c129b"
 
   resource "commander" do
     url "https://rubygems.org/gems/commander-4.4.0.gem"
