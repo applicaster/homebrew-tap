0408011d22ed2d17b64bedd3060f05ea66de5917
Lior Azi <l.azi@applicaster.com>
2017-10-03 22:49:59 +0300

    Update zapptool4.4.0.rb

--- zapptool4.4.0.rb
+++ zapptool4.4.0.rb
@@ -2,10 +2,10 @@ class Zapptool440 < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git,
-                                                 :tag => "v1.1.4"
+                                                 :tag => "v1.1.5"
   head "https://github.com/applicaster/ZappTool.git"
 
-  version "1.1.4"
+  version "1.1.5"
    
   def install
     system "OUTPUT_DIR=#{buildpath} ./scripts/dependencies.sh"
