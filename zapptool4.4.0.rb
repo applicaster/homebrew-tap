f2b83a86c48730ea02b704b3d699fe8be24f9be3
Lior Azi <l.azi@applicaster.com>
2017-10-03 22:26:29 +0300

    Update zapptool4.4.0.rb

--- zapptool4.4.0.rb
+++ zapptool4.4.0.rb
@@ -8,7 +8,7 @@ class Zapptool440 < Formula
   version "1.1.2"
    
   def install
-    system "brew install clac"
+    system "OUTPUT_DIR=#{buildpath} ./scripts/dependencies.sh"
     system "OUTPUT_DIR=#{buildpath} ./scripts/build.sh"
     File.rename("#{buildpath}/build/zapptool", "#{buildpath}/build/zapptool4.4.0")
     prefix.install Dir["#{buildpath}/build/zapptool4.4.0"]
