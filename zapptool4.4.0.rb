83bdca4a11e1a23a398ce7ab0250f799202306b6
kononenkoAnton <a.kononenko@applicaster.com>
2017-10-03 14:20:28 -0400

    Update zapptool4.4.0.rb

--- zapptool4.4.0.rb
+++ zapptool4.4.0.rb
@@ -1,4 +1,4 @@
-class Zapptool < Formula
+class Zapptool4.4.0 < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git,
