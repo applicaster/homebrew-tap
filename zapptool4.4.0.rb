0c4527a72fee09aeac20c6f3425c0ce693db94eb
Lior Azi <l.azi@applicaster.com>
2017-10-03 21:49:00 +0300

    Update zapptool4.4.0.rb

--- zapptool4.4.0.rb
+++ zapptool4.4.0.rb
@@ -8,7 +8,6 @@ class Zapptool440 < Formula
   version "1.1.1"
    
   def install
-    system "brew install ImageMagick"
     File.rename("#{buildpath}/build/zapptool", "#{buildpath}/build/zapptool4.4.0")
     prefix.install Dir["#{buildpath}/build/zapptool4.4.0"]
     inner_binary = "#{prefix}/zapptool4.4.0"
