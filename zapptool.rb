a367a3e676e0f5028cf63c5b88ce47536468f76c
kononenkoAnton <a.kononenko@applicaster.com>
2017-10-19 15:24:08 -0400

    Update zapptool.rb

--- zapptool.rb
+++ zapptool.rb
@@ -2,10 +2,10 @@ class Zapptool < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git,
-                                                 :tag => "v1.1.4"
+                                                 :tag => "v1.2.0"
   head "https://github.com/applicaster/ZappTool.git"
 
-  version "1.1.4"
+  version "1.2.0"
   depends_on "ImageMagick" => :build
   depends_on :xcode => "9.0"
 
