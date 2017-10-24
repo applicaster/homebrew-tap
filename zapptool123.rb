3d59415d82546a17fd73b8f36b4b61900dde1fee
LiorApp <l.azi@applicaster.com>
2017-10-24 17:21:55 +0300

    adding zapptool123

---
+++ zapptool123.rb
@@ -0,0 +1,23 @@
+class Zapptool < Formula
+  desc "Fast configuration tool for modular app"
+  homepage "https://github.com/applicaster/ZappTool"
+  url "git@github.com:applicaster/ZappTool.git", :using => :git,
+                                                 :tag => "v1.2.3"
+  head "https://github.com/applicaster/ZappTool.git"
+
+  version "1.2.3"
+  depends_on "ImageMagick" => :build
+  depends_on :xcode => "9.0"
+
+  def install
+    system "OUTPUT_DIR=#{buildpath} ./scripts/build.sh"
+    prefix.install Dir["#{buildpath}/build/zapptool"]
+    inner_binary = "#{prefix}/zapptool"
+    bin.write_exec_script inner_binary
+    chmod 0755, bin/"zapptool"
+  end
+
+  test do
+    system "(#{bin}/zapptool -version; true)"
+  end
+end
