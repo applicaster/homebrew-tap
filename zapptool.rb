da7d3f00c70adaf6fbb1cede210e0651e9b8153d
Lior Azi <l.azi@applicaster.com>
2017-11-02 23:24:29 +0200

    Update to 1.2.6
    + remove ImageMagick dependency

--- zapptool.rb
+++ zapptool.rb
@@ -2,11 +2,10 @@ class Zapptool < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "git@github.com:applicaster/ZappTool.git", :using => :git,
-                                                 :tag => "v1.2.5"
+                                                 :tag => "v1.2.6"
   head "https://github.com/applicaster/ZappTool.git"
 
-  version "1.2.5"
-  depends_on "ImageMagick" => :build
+  version "1.2.6"
   depends_on :xcode => "9.0"
 
   def install
