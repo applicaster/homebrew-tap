02848f4c5971659aafb8f44abe08c063abc6a7b4
Alex Zchut <a.zchut@applicaster.com>
2017-11-12 16:18:33 +0200

    adding ZappTool 1.3.0

--- zapptool.1.3.0.rb
+++ zapptool.1.3.0.rb
@@ -2,6 +2,7 @@ class Zapptool130 < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "https://dl.bintray.com/applicaster-ltd/pods/ZappTool_v1.3.0_dd5bf298a16a3e7e76074e215fdcbb62e5600206.zip"
+  sha256 ""
   version "v1.3.0"
 
   def install
