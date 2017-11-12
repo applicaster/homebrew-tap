e10edbeab784a32167a5d838410ed4a05120a3b5
Alex Zchut <alexzchut@users.noreply.github.com>
2017-11-12 16:01:01 +0200

    Update zapptool.1.3.0.rb

--- zapptool.1.3.0.rb
+++ zapptool.1.3.0.rb
@@ -1,4 +1,4 @@
-class ZappTool130 < Formula
+class Zapptool130 < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "https://dl.bintray.com/applicaster-ltd/pods/ZappTool_v1.3.0_dd5bf298a16a3e7e76074e215fdcbb62e5600206.zip"
