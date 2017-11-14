bf290bb69fd4fb1561016b1fc95f3fb39c050882
CircleCI build <dev_ci@applicaster.com>
2017-11-14 15:24:22 -0800

    Updating ZappTool to version v1.3.1

--- zapptool.rb
+++ zapptool.rb
@@ -1,22 +1,19 @@
 class Zapptool < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
-  url "git@github.com:applicaster/ZappTool.git", :using => :git,
-                                                 :tag => "v1.2.9"
-  head "https://github.com/applicaster/ZappTool.git"
-
-  version "1.2.9"
-  depends_on :xcode => "9.0"
+  url "https://dl.bintray.com/applicaster-ltd/pods/ZappTool_v1.3.1_625820da503bca9a7dfce9745faf8a4ecef9cbec.zip"
+  sha256 "0f4e1dbbe6fd6a8ccb469ce24388e3a05ec9de0882b1df62f5e078779e3d4bc9"
+  version "1.3.1"
 
   def install
-    system "OUTPUT_DIR=#{buildpath} ./scripts/build.sh"
-    prefix.install Dir["#{buildpath}/build/zapptool"]
-    inner_binary = "#{prefix}/zapptool"
-    bin.write_exec_script inner_binary
-    chmod 0755, bin/"zapptool"
+    exec_filename = "#{name}".downcase
+    prefix_filename = "#{prefix}/#{exec_filename}"
+    prefix.install Dir["#{exec_filename}"]
+    bin.write_exec_script "#{prefix_filename}"
+    chmod 0755, bin/"#{exec_filename}"
   end
 
   test do
-    system "(#{bin}/zapptool -version; true)"
+    system "(#{bin}/#{exec_filename} -version; true)"
   end
 end
