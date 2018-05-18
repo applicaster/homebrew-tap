bbced2990c9b0e758efa2a313c8cb7e36fe22148
CircleCI build <dev_ci@applicaster.com>
2018-05-18 09:03:29 -0700

    Updating ZappTool to version v1.8.0

--- zapptool/zapptool@1.8.rb
+++ zapptool/zapptool@1.8.rb
@@ -1,4 +1,4 @@
-class Zapptool < Formula
+class ZapptoolAT18 < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
   url "https://dl.bintray.com/applicaster-ltd/pods/ZappTool_v1.8.0_6ea2f269c2f7ae1347094e0e1ffdf798d8dfccb7.zip"
@@ -6,7 +6,7 @@ class Zapptool < Formula
   version "1.8.0"
 
   def install
-    exec_filename = "#{name}".downcase
+    exec_filename = "zapptool"
     prefix_filename = "#{prefix}/#{exec_filename}"
     prefix.install Dir["#{exec_filename}"]
     bin.write_exec_script "#{prefix_filename}"
