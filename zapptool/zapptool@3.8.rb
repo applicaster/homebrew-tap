519acc29f7573cac75551bbdbb32465367b35861
Alex Zchut <alexzchut@users.noreply.github.com>
2021-01-03 18:33:52 +0200

    Update zapptool@3.8.rb

--- zapptool/zapptool@3.8.rb
+++ zapptool/zapptool@3.8.rb
@@ -1,7 +1,7 @@
 class ZapptoolAT38 < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
-  url "https://dl.bintray.com/applicaster-ltd/pods/ZappTool_v3.8.8.zip"
+  url "https://assets-production.applicaster.com/zapp/helpers/zapptool/3.8.8/ZappTool_v3.8.8.zip"
   sha256 "20859444041e19b8d239f1ddb6240e132dac0a57916c952ca3641b86159c451f"
   version "3.8.8"
 
