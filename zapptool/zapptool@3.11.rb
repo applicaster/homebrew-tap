7b308c868549e125e9b85dc0f40a41e743038825
Alex Zchut <alexzchut@users.noreply.github.com>
2021-01-04 13:24:40 +0200

    Update zapptool@3.11.rb

--- zapptool/zapptool@3.11.rb
+++ zapptool/zapptool@3.11.rb
@@ -1,8 +1,8 @@
 class ZapptoolAT311 < Formula
   desc "Fast configuration tool for modular app"
   homepage "https://github.com/applicaster/ZappTool"
-  url "https://dl.bintray.com/applicaster-ltd/pods/ZappTool_v3.11.2.zip"
-  sha256 "4bf10708ad9445c99236d7bc694e9bfed60ec94af2829273d011104897007fd3"
+  url "https://assets-production.applicaster.com/zapp/helpers/zapptool/3.11.2/ZappTool_v3.11.2.zip"
+  sha256 "c291ec672ee841a1b84cccce1babb8debbcc86f8c5a70a46956a4fb9f9513d32"
   version "3.11.2"
 
   def install
